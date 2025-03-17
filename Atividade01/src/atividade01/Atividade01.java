fd95c7c55102c0ad69629ebc2cfbd26e8c8b4d8c
Arthur Postringer <138155954+ArtPostringer@users.noreply.github.com>
2025-03-17 16:19:08 -0200

    Add files via upload

---
+++ Atividade01/src/atividade01/Atividade01.java
@@ -0,0 +1,12 @@
+
+package atividade01;
+
+public class Atividade01 {
+
+    public static void main(String[] args) {
+        Ex01 e1 = new Ex01();
+        e1.setPalavra("Xiru");
+        System.out.println("A palavra "+e1.getPalavra() +" tem "+e1.ContarPalavra()+" letras!");
+    }
+    
+}
