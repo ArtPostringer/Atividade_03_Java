5a6e2a311067ff6fd8898e2de1ac5475ccae51f2
Arthur Postringer <138155954+ArtPostringer@users.noreply.github.com>
2025-03-19 16:50:47 -0300

    Add files via upload

---
+++ Atividade04/src/atividade04/Atividade04.java
@@ -0,0 +1,35 @@
+package atividade04;
+
+import java.util.Scanner;
+
+public class Atividade04 {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            Ex04 calc = new Ex04();
+
+            System.out.print("Digite um número: ");
+            calc.setNumero(scanner.nextDouble());
+
+            System.out.println("Número: " + calc.getNumero());
+            System.out.print("Digite um valor para somar: ");
+            System.out.println("Soma: " + calc.somar(scanner.nextDouble()));
+
+            System.out.print("Digite um valor para subtrair: ");
+            System.out.println("Subtração: " + calc.subtrair(scanner.nextDouble()));
+
+            System.out.print("Digite um valor para multiplicar: ");
+            System.out.println("Multiplicação: " + calc.multiplicar(scanner.nextDouble()));
+
+            System.out.print("Digite um valor para dividir: ");
+            System.out.println("Divisão: " + calc.dividir(scanner.nextDouble()));
+
+            System.out.println("Raiz Quadrada: " + calc.raizQuadrada());
+
+            System.out.print("Digite um expoente para calcular a potência: ");
+            System.out.println("Potência: " + calc.potencia(scanner.nextDouble()));
+
+            System.out.println("Fatorial: " + calc.fatorial());
+        }
+    }
+}
